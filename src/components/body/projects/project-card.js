de5c2a06dbe732bbf0682edfbe1a052f3203880c
Your Name <you@example.com>
2022-02-22 10:40:29 +0530

    Fifth commit

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -5,7 +5,7 @@ const ProjectCard = ({ project }) => {
     <div className="project-card">
       <div className="project-info">
         <label className="project-title">{project.title}</label>
-        <div className="project-link">
+        <div className="project-links">
           {project.demo && (
             <a className="project-link" href={project.demo}>
               <div className="link-button">
@@ -13,6 +13,19 @@ const ProjectCard = ({ project }) => {
               </div>
             </a>
           )}
+          {project.github && (
+            <a className="project-link" href={project.github}>
+              <div className="link-button">
+                <i class="devicon-github-original colored"></i>Github
+              </div>
+            </a>
+          )}
+        </div>
+        <p>{project.about}</p>
+        <div className="project-tags">
+          {project.tags.map((tag) => {
+            return <label className="tag">{tag}</label>;
+          })}
         </div>
       </div>
       <img src={project.image} className="project-photo" />
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,12 +1,21 @@
 export const ProjectData = [
   {
     id: 1,
-    title: "random projects 2",
-    about: "sjAS DAD AWE",
-    tags: ["React.js", "JavaScripts"],
-    demo: "",
-    github: "",
+    title: "Icecream Factory",
+    about: "This app is about icecream factory made using react.js ",
+    tags: ["React.js", "JavaScripts", "Html & Css", "Bootstrap"],
+    demo: "#",
+    github: "https://github.com/shriikant7/React",
     image:
       "https://www.hellowonderful.co/ckfinder/userfiles/images/12-diy-ice-cream-cones(1).jpg",
+  },
+  {
+    id: 2,
+    title: "ToDo app",
+    about: "This app is about ToDo list ",
+    tags: ["JavaScripts", "Html & Css"],
+    demo: "#",
+    github: "https://github.com/shriikant7/Todo-app-",
+    image: "https://wallpaperaccess.com/full/1489353.jpg",
   },
 ];
