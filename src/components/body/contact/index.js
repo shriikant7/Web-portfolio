088ef0ac9fc570821fb49002445639eeed4ae0c4
Your Name <you@example.com>
2022-02-23 12:01:35 +0530

    Seventh commit

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -1,4 +1,25 @@
+import react from "react";
+import Seperator from "../../common/social-contacts/seperator";
+import SocialContact from "../../common/social-contacts";
+import "./contact.css";
 const Contact = () => {
-  return <div>this is contact</div>;
+  return (
+    <div className="contact">
+      <Seperator />
+      <label className="section-title">Contacts</label>
+      <div className="contact-container">
+        <div className="contact-left">
+          <p>Feel free to connect with me on social media! </p>
+          <SocialContact />
+        </div>
+        <div className="download">
+          <a download href={require("../../../assets/ShrikantMule.pdf")}>
+            <i class="fi fi-rr-download download-icon"></i>
+            Download Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Contact;
--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,22 +1,22 @@
 export const SocialData = [
   {
     platform: "instagram",
-    link: "",
+    link: "https://www.instagram.com/shriikant7/",
     icon: require("../../assets/icons/instagram.png"),
   },
   {
     platform: "linkedin",
-    link: "",
+    link: "https://www.linkedin.com/in/shrikant-mule-1203351bb",
     icon: require("../../assets/icons/linkedin.png"),
   },
   {
     platform: "github",
-    link: "",
+    link: "https://github.com/shriikant7",
     icon: require("../../assets/icons/github.png"),
   },
   {
     platform: "facebook",
-    link: "",
+    link: "https://www.facebook.com/profile.php?id=100005228569005",
     icon: require("../../assets/icons/facebook.png"),
   },
 ];
